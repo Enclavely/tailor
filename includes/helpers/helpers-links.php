8a21963777cd2e5d06b737093d55cdf132fc70b5
Harry Green <harry@hgreen.co.uk>
2016-09-01 10:57:04 +0100

    Add edit link above content editor (#28)
    * Add edit link above content editor. Fixes #23
    
    * Prevent content editor link being displayed when tailoring a page

--- includes/helpers/helpers-links.php
+++ includes/helpers/helpers-links.php
@@ -18,7 +18,7 @@ if ( ! function_exists( 'tailor_admin_bar_edit_link' ) ) {
 	 * @param WP_Admin_Bar $wp_admin_bar
 	 */
 	function tailor_admin_bar_edit_link( $wp_admin_bar ) {
-		
+
 		if ( tailor()->is_canvas() ) {
 			return;
 		}
@@ -81,4 +81,53 @@ if ( ! function_exists( 'tailor_inline_edit_link' ) ) {
 
 	add_filter( 'page_row_actions', 'tailor_inline_edit_link', 10, 2 );
 	add_filter( 'post_row_actions', 'tailor_inline_edit_link', 10, 2 );
+}
+
+if ( ! function_exists( 'tailor_content_editor_link' ) ) {
+
+    /**
+     * Adds an edit link above content editor, next to the 'Add Media' button
+     *
+     * @since 1.5.4
+     */
+    function tailor_content_editor_link() {
+
+		if ( tailor()->is_canvas() || tailor()->is_tailoring() ) {
+			return;
+		}
+
+		// Only add the Admin Bar link when editing a post or page
+		if ( is_admin() ) {
+
+			$screen = get_current_screen();
+			if ( 'post' !== $screen->base ) {
+				return;
+			}
+		}
+
+		// Do not display link on archive pages
+		else if ( ! is_singular() ) {
+			return;
+		}
+
+		$post = get_post();
+		if ( ! $post || 'auto-draft' == get_post_status( $post->ID ) ) {
+			return;
+		}
+
+		if ( tailor()->check_user_role() && tailor()->check_post( $post ) ) {
+
+			$post_type_object = get_post_type_object( get_post_type( $post ) );
+
+			$link = array(
+				'href'  => tailor()->get_edit_url( $post->ID ),
+				'id'    => 'edit-with-tailor',
+				'title' => sprintf( __( 'Tailor this %s', 'tailor' ), $post_type_object->labels->singular_name ),
+			);
+
+			echo vsprintf( '<a href="%s" id="%s" class="button">%s</a>', $link );
+		}
+	}
+
+	add_action( 'media_buttons', 'tailor_content_editor_link', 99 );
 }
